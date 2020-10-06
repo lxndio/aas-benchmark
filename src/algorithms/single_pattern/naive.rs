a241857020d3337183cb7b016fe0c7dc115e2fd1
Alexander Korn <a@lxnd.me>
2020-10-06 14:04:23 +0200

    Add Cargo project and naive algorithm for single pattern matching

---
+++ src/algorithms/single_pattern/naive.rs
@@ -0,0 +1,22 @@
+/// The naive algorithm approach uses a simple loop to look for an occurrence
+/// of a `pattern` in a `text`.
+/// 
+/// It does this by iterating over each index `i` of the text's characters
+/// starting at index `i0` and comparing the following `m` characters with
+/// the pattern, `m` being the length of the pattern.
+/// 
+/// After an occurrence has been found, the algorithm returns the index
+/// marking the first character of the occurrence and therefore terminates.
+/// If the pattern could not be found in the text, `None` is returned.
+pub fn naive(pattern: &[u8], text: &[u8], i0: usize) -> Option<usize> {
+    let m = pattern.len();
+    let n = text.len();
+
+    for i in i0..(n - m + 1) {
+        if &text[i..i + m] == pattern {
+            return Some(i);
+        }
+    }
+
+    None
+}
