0312efada30b33f5b6816a493bf282f4dd2da747
Alexander Korn <a@lxnd.me>
2020-10-08 22:40:51 +0200

    Add cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,58 @@
+#[macro_use]
+extern crate clap;
+
 mod algorithms;
 mod generate;
 mod measure;
+
+use clap::App;
 
 use algorithms::single_pattern::naive::naive_all;
 use generate::gen_rand_bytes;
 use measure::{calculate_avg_duration, measure_multiple};
 
+pub const ALGORITHMS: [&str; 1] = ["Naive"]; // TODO necessary?
+
 fn main() {
-    let text = gen_rand_bytes(1_000_000);
-    let pattern = &text[20..25];
+    let clap_yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(clap_yaml).get_matches();
 
-    let durations = measure_multiple(pattern, &text, naive_all, 10);
+    let algorithm = matches
+        .value_of("ALGORITHM")
+        .unwrap_or("NonexistentAlgorithm");
+    let executions: usize = matches
+        .value_of("executions")
+        .unwrap_or("1")
+        .parse()
+        .unwrap_or(0);
 
-    println!("Duration: {:?}", durations);
-    println!("Average: {:?}", calculate_avg_duration(durations));
+    let mut failure = false;
+
+    if executions == 0 {
+        println!("The -n argument needs to be a positive integer greater than 0.");
+        failure = true;
+    }
+
+    let algorithm_fn = match algorithm.to_lowercase().as_str() {
+        "naive" => Some(naive_all),
+        _ => {
+            println!("Unknown algorithm given.");
+            failure = true;
+
+            None
+        }
+    };
+
+    if !failure {
+        let text = gen_rand_bytes(1_000_000);
+        let pattern = &text[20..25];
+
+        // Unwrap is safe here because of the failure variable
+        let durations = measure_multiple(pattern, &text, algorithm_fn.unwrap(), executions);
+
+        if durations.len() != 0 {
+            println!("Duration: {:?}", durations);
+            println!("Average: {:?}", calculate_avg_duration(durations));
+        }
+    }
 }
