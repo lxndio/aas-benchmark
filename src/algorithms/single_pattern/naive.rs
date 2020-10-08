2e73794f5e6f6479aed2d71626cfca81977a5b6a
Alexander Korn <a@lxnd.me>
2020-10-08 17:49:31 +0200

    Rust format

--- src/algorithms/single_pattern/naive.rs
+++ src/algorithms/single_pattern/naive.rs
@@ -1,10 +1,10 @@
 /// The naive algorithm approach uses a simple loop to look for an occurrence
 /// of a `pattern` in a `text`.
-/// 
+///
 /// It does this by iterating over each index `i` of the text's characters
 /// starting at index `i0` and comparing the following `m` characters with
 /// the pattern, `m` being the length of the pattern.
-/// 
+///
 /// After an occurrence has been found, the algorithm returns the index
 /// marking the first character of the occurrence and therefore terminates.
 /// If the pattern could not be found in the text, `None` is returned.
