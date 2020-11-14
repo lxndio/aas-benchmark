4eb73fecb539e19a35318409f9f96d33af2268f5
Alexander Korn <a@lxnd.me>
2020-11-14 04:20:22 +0100

    Fix typo in comment

--- src/measure/mod.rs
+++ src/measure/mod.rs
@@ -8,7 +8,7 @@ use crate::measure::measure_result::MeasureResult;
 
 /// A single measurement containing an optional preparation runtime,
 /// a mandatory execution runtime (of the actual pattern matching algorithm
-/// itself) and the number of matches, i. d. how often the pattern has been
+/// itself) and the number of matches, i. e. how often the pattern has been
 /// found in the text.
 pub type SingleMeasurement = (Option<Duration>, Duration, usize);
 
