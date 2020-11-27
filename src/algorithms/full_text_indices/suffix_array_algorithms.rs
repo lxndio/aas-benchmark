b0241af53e1c6473cbf2e0b7be40a7113b4289a8
Alexander Korn <a@lxnd.me>
2020-11-27 20:06:53 +0100

    Pattern matching using suffix array is now a lot faster by using binary search, fixes #18

--- src/algorithms/full_text_indices/suffix_array_algorithms.rs
+++ src/algorithms/full_text_indices/suffix_array_algorithms.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::time::SystemTime;
 
 use crate::match_algorithm::SuffixArrayAlgorithm;
@@ -38,48 +39,112 @@ impl Measurement for SuffixArrayAlgorithm {
     }
 }
 
+/// Returns occurrences of a pattern in a text using a predefined suffix array.
+///
+/// The algorithm uses a modified binary search to find an interval in the
+/// given suffix array including those suffixes which have a prefix equal
+/// to the sought pattern. Using that interval, it then extracts the beginning
+/// positions of the occurrences of the pattern in the text from the suffix array.
 pub fn match_pattern(pos: Vec<usize>, pattern: &[u8], text: &[u8]) -> Vec<usize> {
-    let m = pattern.len();
-    let n = text.len();
+    // Define the binary search function as a local function
+    // because it is only needed here
+    fn binary_search(
+        pos: &[usize],
+        pattern: &[u8],
+        text: &[u8],
+        mut l: usize,
+        mut r: usize,
+    ) -> (usize, usize) {
+        // At the beginning, the left interval bound is set as high as possible,
+        // while the right bound is set as low as possible; this allows the
+        // min and max functions below to be used to replace those bounds with
+        // the correct ones or to detect that the interval is empty, if the left
+        // bound is still larger than the right bound at the end
+        let mut interval_l: usize = pos.len() - 1;
+        let mut interval_r: usize = 0;
 
-    // let l: usize = pos.iter().position(|x| less_eq_m(pattern, &text[*x..], m)).unwrap_or(n);
-    // let r: isize = pos.iter().rev().position(|x| greater_eq_m(pattern, &text[*x..], m)).map(|x| x as isize).unwrap_or(-1);
+        // This is a normal binary search apart from the equal case
+        while l <= r {
+            let m = l + ((r - l) / 2);
+            // Let prefix_n be the prefix of the pattern's length of the suffix
+            // at the m-th position in the suffix array; can be shorter if the
+            // suffix's length is shorter than the pattern's length
+            let prefix_n = text[pos[m]..].iter().take(pattern.len());
 
-    let mut ls: Vec<usize> = (0..n)
-        .filter(|r| less_eq_m(pattern, &text[pos[*r]..], m))
-        .collect();
-    ls.push(n);
+            match prefix_n.cmp(pattern) {
+                Ordering::Less => l = m + 1,
+                Ordering::Greater => r = m - 1,
+                Ordering::Equal => {
+                    // As a match has now been found, include it in the interval
+                    // by settings the according bounds to include the current value
+                    if m < interval_l {
+                        interval_l = m
+                    }
+                    if m > interval_r {
+                        interval_r = m
+                    }
 
-    let mut rs: Vec<isize> = (0..n)
-        .filter(|r| greater_eq_m(pattern, &text[pos[*r]..], m))
-        .map(|x| x as isize)
-        .collect();
-    rs.push(-1);
+                    // Because there can be multiple positions matching the
+                    // search criterion, search to the left and to the right
+                    // for more occurrences
+                    let (l1, _) = binary_search(pos, pattern, text, l, m - 1);
+                    let (_, r2) = binary_search(pos, pattern, text, m + 1, r);
 
-    let l = *ls.iter().min().unwrap();
-    let r = *rs.iter().max().unwrap();
+                    // Now take the left-most left interval bound and
+                    // the right-most right interval bound
+                    interval_l = l1.min(interval_l);
+                    interval_r = r2.max(interval_r);
 
-    if r >= l as isize {
-        (l..=r as usize).map(|x| pos[x]).collect()
+                    return (interval_l, interval_r);
+                }
+            }
+        }
+
+        (interval_l, interval_r)
+    }
+
+    // Call the binary search function over the entire suffix array
+    let (l, r) = binary_search(&pos, pattern, text, 0, pos.len() - 1);
+
+    // Return empty vector if interval is empty (right bound is lower than left bound)
+    if l <= r {
+        pos[l..=r].to_vec()
     } else {
         Vec::new()
     }
 }
 
-fn less_eq_m(a: &[u8], b: &[u8], m: usize) -> bool {
-    let mut m = m;
-    if a.len().min(b.len()) < m {
-        m = a.len().min(b.len());
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::algorithms::full_text_indices::sais::fast;
+    use crate::algorithms::single_pattern::naive::naive_all;
+    use crate::generate::{gen_rand_bytes, rand_pattern_from_bytes};
+
+    #[test]
+    fn test_match_pattern() {
+        for i in 0..10 {
+            println!("Test {}", i);
+
+            let mut text = gen_rand_bytes(1_000_000, None);
+            text.push(0);
+            let text = text.as_slice();
+
+            let pattern = rand_pattern_from_bytes(text, 2, None);
+
+            let pos = fast(text);
+
+            let mut matches1 = match_pattern(pos, pattern, text);
+            let mut matches2 = naive_all(pattern, text);
+
+            // Sort both lists because order could be different resulting
+            // in a failure of this test
+            matches1.sort_unstable();
+            matches2.sort_unstable();
+
+            println!("{:?}, {:?}", matches1, matches2);
+
+            assert_eq!(matches1, matches2);
+        }
     }
-
-    a[0..m] <= b[0..m]
-}
-
-fn greater_eq_m(a: &[u8], b: &[u8], m: usize) -> bool {
-    let mut m = m;
-    if a.len().min(b.len()) < m {
-        m = a.len().min(b.len());
-    }
-
-    a[0..m] >= b[0..m]
 }
